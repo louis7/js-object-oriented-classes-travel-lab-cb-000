0699a2b82ffd4d40a2fc0fa158135ae2e2603827
louis baptiste <louisvis@ymail.com>
2020-03-24 19:33:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,9 +13,10 @@ class Driver {
   constructor(name, startDate, endyear ) {
     const Endyear = new date(endyear)
     this.name = name;
-    this.startDate = new Date(startDate) ;
-    this.yearsExperienceFromBeginningOf = ${this.startDate} - Endyear;
-
   }
-
-}
+    yearsExperienceFromBeginningOf( year ) {
+     let endDate = new Date( year, 1, 1 );
+     let totalYears = ( endDate - this.startDate ) / ( 365 * 24 * 60 * 60 * 1000 );
+     return parseInt( totalYears );
+   }
+ }
